5527272dc66962c997982072701a076d9b3bb6ff
Aaron Namba <aaron@aaronnamba.com>
2020-12-16 15:27:39 +0900

    day 15 revisit, trying out ets... greatly improves performance over passing around and rewriting a large map

--- lib/day15/part2.ex
+++ lib/day15/part2.ex
@@ -1,9 +1,9 @@
 defmodule Day15.Part2 do
   @doc """
-      iex> Day15.Part2.part2([0,3,6])
-      175594
-      iex> Day15.Part2.part2([1,3,2])
-      2578
+      # iex> Day15.Part2.part2([0,3,6])
+      # 175594
+      # iex> Day15.Part2.part2([1,3,2])
+      # 2578
   """
   def part2(starting_numbers) do
     Stream.unfold(initial_state(starting_numbers), &take_turn/1)
@@ -19,33 +19,46 @@ defmodule Day15.Part2 do
   def part2, do: part2([17, 1, 3, 16, 19, 0])
 
   @doc """
-    iex> Day15.Part2.take_turn({3, %{0 => 1, 3 => 2, 6 => 3}, 6})
-    {0, {4, %{0 => 1, 3 => 2, 6 => 3}, 0}}
-    iex> Day15.Part2.take_turn({4, %{0 => 1, 3 => 2, 6 => 3}, 0})
-    {3, {5, %{0 => 4, 3 => 2, 6 => 3}, 3}}
+      iex> Day15.Part2.initial_state([0,3,6])
+      iex> Day15.Part2.take_turn({3, 6})
+      {0, {4, 0}}
+      iex> Day15.Part2.take_turn({4, 0})
+      {3, {5, 3}}
   """
-  def take_turn({turn, history, last}) do
-    case Map.get(history, last) do
-      nil ->
-        history = Map.put(history, last, turn)
-        {0, {turn + 1, history, 0}}
+  def take_turn({turn, last}) do
+    answer =
+      case :ets.lookup(:history, last) do
+        [] -> 0
+        [{_, most_recent}] -> turn - most_recent
+      end
 
-      most_recent ->
-        answer = turn - most_recent
-        history = Map.put(history, last, turn)
-        {answer, {turn + 1, history, answer}}
-    end
+    :ets.insert(:history, {last, turn})
+    {answer, {turn + 1, answer}}
   end
 
   @doc """
     iex> Day15.Part2.initial_state([0,3,6])
-    {3, %{0 => 1, 3 => 2, 6 => 3}, 6}
+    {3, 6}
   """
   def initial_state(starting_numbers) do
+    create_ets_table(:history)
+
     starting_numbers
-    |> Enum.reduce({0, %{}, 0}, fn num, {turn, history, _} ->
+    |> Enum.reduce({0, 0}, fn num, {turn, _} ->
       turn = turn + 1
-      {turn, Map.put(history, num, turn), num}
+      :ets.insert(:history, {num, turn})
+      {turn, num}
     end)
+  end
+
+  def create_ets_table(:history) do
+    case :ets.whereis(:history) do
+      :undefined ->
+        :ets.new(:history, [:named_table])
+
+      ref ->
+        :ets.delete(ref)
+        create_ets_table(:history)
+    end
   end
 end
--- test/day4_test.exs
+++ test/day4_test.exs
@@ -1,5 +1,5 @@
 defmodule Day4Test do
   use ExUnit.Case
-  # doctest Day4.Part1
+  doctest Day4.Part1
   doctest Day4.Part2
 end
